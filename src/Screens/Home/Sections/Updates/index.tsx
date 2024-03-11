d19758ab45be1cf3d2a1b5654d0d12862694156c
Adnan Husain <adnan@mithyalabs.com>
2024-03-11 10:52:24 +0530

    padding fix

--- src/Screens/Home/Sections/Updates/index.tsx
+++ src/Screens/Home/Sections/Updates/index.tsx
@@ -15,7 +15,7 @@ const Updates: FC<IUpdatesProps> = () => {
     <MaxWidthWrapper containerClassName="" className="pb-10 ">
       <Center className="mx-auto max-w-5xl py-8 gap-10 flex-col">
         <h1>Latest KC Updates</h1>
-        <div className="w-full overflow-x-auto no-scrollbar">
+        <div className="w-full overflow-x-auto no-scrollbar px-2">
           <div className="flex gap-8 w-[1000px] md:w-auto h-[480px] py-2">
             <div className="p-6 flex flex-col shadow-md bg-[#FDAF4D] w-[80vw] md:w-auto md:basis-[45%] rounded-lg h-full items-start">
               <h1 className="text-left">We’re Hiring!</h1>
